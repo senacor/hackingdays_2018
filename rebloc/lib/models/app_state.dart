c33398558c53e5162c0377d11acd5857b3f321a2
Markus Dobel <markus.dobel@senacor.com>
2018-11-23 14:17:19 +0100

    refactor firebase to a separate holder class

--- rebloc/lib/models/app_state.dart
+++ rebloc/lib/models/app_state.dart
@@ -1,11 +1,10 @@
+import 'package:better_yunar/models/achievement.dart';
+import 'package:better_yunar/models/loyaltyCard.dart';
+import 'package:better_yunar/models/onboardingScreen.dart';
+import 'package:better_yunar/models/user.dart';
 import 'package:built_collection/built_collection.dart';
 import 'package:built_value/built_value.dart';
 import 'package:built_value/serializer.dart';
-import 'package:better_yunar/models/loyaltyCard.dart';
-import 'package:better_yunar/models/user.dart';
-import 'package:better_yunar/models/onboardingScreen.dart';
-import 'package:better_yunar/models/achievement.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
 
 part 'app_state.g.dart';
 
@@ -21,39 +20,7 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
   AppState._();
 
   factory AppState.initialState() {
-    final AppState result = AppState();
-    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
-
-    _firebaseMessaging.configure(
-      onMessage: (Map<String, dynamic> message) async {
-        print("onMessage: $message");
-//        _showItemDialog(message);
-      },
-      onLaunch: (Map<String, dynamic> message) async {
-        print("onLaunch: $message");
-//        _navigateToItemDetail(message);
-      },
-      onResume: (Map<String, dynamic> message) async {
-        print("onResume: $message");
-//        _navigateToItemDetail(message);
-      },
-    );
-
-    _firebaseMessaging.requestNotificationPermissions(
-        const IosNotificationSettings(sound: true, badge: true, alert: true));
-
-    _firebaseMessaging.onIosSettingsRegistered
-        .listen((IosNotificationSettings settings) {
-      print("Settings registered: $settings");
-    });
-
-    _firebaseMessaging.getToken().then((String token) {
-          assert (token != null);
-          print("Token received: $token");
-        }
-    );
-
-    return result;
+    return AppState();
   }
 
   factory AppState([updates(AppStateBuilder b)]) = _$AppState;
