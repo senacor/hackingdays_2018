171114a1437e5686b5b082fc002257ce45aeaded
Marlon Alagoda <marlon@alagoda.at>
2018-11-23 11:31:53 +0100

    Readded markus firabse stuff

--- rebloc/lib/models/app_state.dart
+++ rebloc/lib/models/app_state.dart
@@ -12,8 +12,6 @@ part 'app_state.g.dart';
 abstract class AppState implements Built<AppState, AppStateBuilder> {
   static Serializer<AppState> get serializer => _$appStateSerializer;
 
-  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();	
-  String _firebaseToken;
   BuiltMap<String, LoyaltyCard> get loyaltyCards;
   BuiltMap<String, Achievement> get achievements;
   @nullable
@@ -24,32 +22,35 @@ abstract class AppState implements Built<AppState, AppStateBuilder> {
 
   factory AppState.initialState() {
     final AppState result = AppState();
-    final FirebaseMessaging _firebaseMessaging = result._firebaseMessaging;	
-     _firebaseMessaging.configure(	
-      onMessage: (Map<String, dynamic> message) async {	
-        print("onMessage: $message");	
-//        _showItemDialog(message);	
-      },	
-      onLaunch: (Map<String, dynamic> message) async {	
-        print("onLaunch: $message");	
-//        _navigateToItemDetail(message);	
-      },	
-      onResume: (Map<String, dynamic> message) async {	
-        print("onResume: $message");	
-//        _navigateToItemDetail(message);	
-      },	
-    );	
-     _firebaseMessaging.requestNotificationPermissions(	
-        const IosNotificationSettings(sound: true, badge: true, alert: true));	
-     _firebaseMessaging.onIosSettingsRegistered	
-        .listen((IosNotificationSettings settings) {	
-      print("Settings registered: $settings");	
-    });	
-     _firebaseMessaging.getToken().then((String token) {	
-          assert (token != null);	
-          print("Token received: $token");	
-          result._firebaseToken = token;	
-        }	
+    final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: $message");
+//        _showItemDialog(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+//        _navigateToItemDetail(message);
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+//        _navigateToItemDetail(message);
+      },
+    );
+
+    _firebaseMessaging.requestNotificationPermissions(
+        const IosNotificationSettings(sound: true, badge: true, alert: true));
+
+    _firebaseMessaging.onIosSettingsRegistered
+        .listen((IosNotificationSettings settings) {
+      print("Settings registered: $settings");
+    });
+
+    _firebaseMessaging.getToken().then((String token) {
+          assert (token != null);
+          print("Token received: $token");
+        }
     );
 
     return result;
